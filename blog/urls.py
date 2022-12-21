b606de73577dba2882cd8ec86576c465fad45198
gioZAK <giovane.zakc@gmail.com>
2022-12-21 00:34:27 +0000

    Connect view to the url path and create the editpost template

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path("", views.PostList.as_view(), name="home"),
     path('create/', views.create_post, name='create_post'),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
+    path('edit/<slug:slug>/', views.edit_post, name='edit_post'),
     path('like/<slug:slug>', views.PostLike.as_view(), name='post_like'),
 ]
