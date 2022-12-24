3de0314092bd328ff40b0bddc569d91d2152e328
gioZAK <giovane.zakc@gmail.com>
2022-12-24 11:53:05 +0000

    Set up the new url path for the edit profile template

--- userprofile/urls.py
+++ userprofile/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('<str:username>/', views.ProfileView.as_view(), name='profile'),
     path('account/delete/', views.DeleteAccountView.as_view(),
          name='delete_account'),
+    path('edit/<str:username>/', views.EditProfileView.as_view(), name='edit')
 ]
