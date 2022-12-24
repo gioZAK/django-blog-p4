f9f54b6452aae6d9aeb94c083a07e524742178dc
gioZAK <giovane.zakc@gmail.com>
2022-12-24 11:43:46 +0000

    Setup new url path to handle the new route for the profile app

---
+++ userprofile/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('<str:username>/', views.ProfileView.as_view(), name='profile'),
+    path('account/delete/', views.DeleteAccountView.as_view(),
+         name='delete_account'),
+]
